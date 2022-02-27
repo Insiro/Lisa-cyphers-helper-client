bc25c7c7c4127336d5b70050a47f58abd1d622f2
Insiro <leeon@insiro.me>
2022-02-27 20:04:23 +0900

    remove: cli, pagetrait, command on history

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
-// pub mod cli;
-// pub mod client;
-// pub mod command;
-// pub mod error;
-// pub mod object;
-// pub mod page;
+pub mod client;
+pub mod error;
+pub mod object;
+pub mod page;
 // pub mod servicer;
-// pub mod util;
-// extern crate getset;
-
+pub mod util;
 pub mod gui;
--- src/util/history.rs
+++ src/util/history.rs
@@ -1,6 +1,3 @@
-use crate::command::Command;
-use crate::util::history;
-
 pub enum Kind {
     Main,
     Matches,
@@ -21,15 +18,14 @@ impl Kind {
 }
 
 pub mod new {
+    use crate::page::Page;
+
     pub struct History {
         key: Option<String>,
-        page: Box<dyn PageTrait>,
+        page: Page,
     }
-    pub trait PageTrait {
-        fn to_str(&self) -> &str;
-        fn from_key(&mut self, key: &str);
-    }
-    pub struct Histories(Vec<Box<dyn PageTrait>>);
+    
+    pub struct Histories(Vec<History>);
     impl Histories {
         pub fn new() -> Self {
             Histories(vec![])
@@ -37,17 +33,17 @@ pub mod new {
         pub fn len(&self) -> usize {
             self.0.len()
         }
-        pub fn insert(&mut self, history: Box<dyn PageTrait>) {
+        pub fn insert(&mut self, history: History) {
             self.0.push(history);
         }
-        pub fn pop(&mut self) -> Option<Box<dyn PageTrait>> {
+        pub fn pop(&mut self) -> Option<History> {
             self.0.pop()
         }
     }
 }
 
 pub struct History {
-    page: history::Kind,
+    page: Kind,
     key: Option<String>,
 }
 pub fn init() -> Histories {
@@ -64,13 +60,6 @@ impl Histories {
     pub fn len(&self) -> usize {
         self.list.len()
     }
-    pub fn cli_point(&self) -> String {
-        let mut re = String::new();
-        for item in self.list.iter() {
-            re.push_str(item.cli_page_name().as_str());
-        }
-        re
-    }
     pub fn push(&mut self, history: History) {
         self.list.push(history);
     }
@@ -84,24 +73,7 @@ impl Histories {
     pub fn get_curr(&self) -> Option<&History> {
         self.list.last()
     }
-    pub fn new(page: history::Kind, key: Option<String>) -> History {
+    pub fn new(page: Kind, key: Option<String>) -> History {
         History { page, key }
-    }
-    pub fn run_cli(&mut self) -> Command {
-        //run last page as cli
-        Command::NotImpletated
-    }
-}
-
-impl History {
-    pub fn cli_page_name(&self) -> String {
-        match &self.page {
-            Kind::Main => "> ".to_string(),
-            Kind::Matches => "match > ".to_string(),
-            _ => match &self.key {
-                None => format!("> {}> ", self.page.as_str()),
-                Some(key) => format!("> {} {}", self.page.as_str(), key),
-            },
-        }
     }
 }
