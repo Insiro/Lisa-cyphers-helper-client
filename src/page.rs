834a2d5c326fdee506f5b65ba588c214cde7f67a
Insiro <leeon@insiro.me>
2022-02-27 19:48:29 +0900

    refactor: remove cli on record and rename

--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,4 @@
-pub mod game_record;
 pub mod matches;
-pub mod my;
 pub mod profile;
+pub mod record;
 pub mod set;
---
+++ src/page/record.rs
@@ -0,0 +1,70 @@
+use std::io;
+
+use crate::object::player;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct GameRecord {
+    player: player::Info,
+    records: Vec<GameInfo>,
+}
+impl GameRecord {
+    pub fn get_player(&mut self) -> &player::Info {
+        &self.player
+    }
+    pub fn get_records(&mut self) -> &Vec<GameInfo> {
+        &self.records
+    }
+}
+pub fn dumy() -> GameRecord {
+    GameRecord {
+        player: player::Info::dumy(),
+        records: vec![],
+    }
+}
+pub fn search(_id: &str) -> Option<GameRecord> {
+    //TODO: search by api
+    Some(dumy())
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum GameType {
+    Normal,
+    Rating,
+}
+
+impl GameType {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            GameType::Normal => "Normal",
+            GameType::Rating => "Rating",
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GameInfo {
+    match_id: String,
+    game_type: GameType,
+    is_win: bool,
+}
+impl GameInfo {
+    pub fn dumy() -> GameInfo {
+        GameInfo {
+            match_id: "".to_string(),
+            game_type: GameType::Normal,
+            is_win: true,
+        }
+    }
+    pub fn get_type(&self) -> &GameType {
+        &self.game_type
+    }
+    pub fn win(self) -> bool {
+        self.is_win
+    }
+    pub fn get_id(&self) -> &String {
+        &self.match_id
+    }
+}
+
+pub fn help(_args: Vec<String>) {}
